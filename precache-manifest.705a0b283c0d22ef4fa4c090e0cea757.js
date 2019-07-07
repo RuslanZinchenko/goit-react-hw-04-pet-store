7e6102445dcddb29cb76d5d29a7336c6287d0bcf
RuslanZinchenko <zinchenko.rusya@gmail.com>
2019-07-08 00:34:54 +0300

    Updates

--- precache-manifest.705a0b283c0d22ef4fa4c090e0cea757.js
+++ precache-manifest.705a0b283c0d22ef4fa4c090e0cea757.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "57b613c8e6cf926429dc043f594fce98",
+    "revision": "cf9e41e5fe30ed5188afb8fc5a84df78",
     "url": "/goit-react-hw-04-pet-store/index.html"
   },
   {
@@ -20,11 +20,11 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-04-pet-store/static/css/main.1962c9e3.chunk.css"
   },
   {
-    "revision": "25b47869ea367d1ca6ab",
+    "revision": "bd26e41f6912b4e10db0",
     "url": "/goit-react-hw-04-pet-store/static/css/pet-page.5575ecf8.chunk.css"
   },
   {
-    "revision": "edff630f687c9ee85e19",
+    "revision": "479985c7725c11317dfa",
     "url": "/goit-react-hw-04-pet-store/static/css/pets-page.ac825bc3.chunk.css"
   },
   {
@@ -44,15 +44,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-04-pet-store/static/js/main.612c96e1.chunk.js"
   },
   {
-    "revision": "25b47869ea367d1ca6ab",
-    "url": "/goit-react-hw-04-pet-store/static/js/pet-page.37755372.chunk.js"
+    "revision": "bd26e41f6912b4e10db0",
+    "url": "/goit-react-hw-04-pet-store/static/js/pet-page.c1cd75db.chunk.js"
   },
   {
-    "revision": "edff630f687c9ee85e19",
-    "url": "/goit-react-hw-04-pet-store/static/js/pets-page.5305c8a5.chunk.js"
+    "revision": "479985c7725c11317dfa",
+    "url": "/goit-react-hw-04-pet-store/static/js/pets-page.d28379ca.chunk.js"
   },
   {
-    "revision": "8cf08f341b5163b45aa1",
-    "url": "/goit-react-hw-04-pet-store/static/js/runtime~main.1ba07b5f.js"
+    "revision": "833792765f6069e27eb8",
+    "url": "/goit-react-hw-04-pet-store/static/js/runtime~main.f2815089.js"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-04-pet-store/precache-manifest.607e147f74fe7d09097c7e7a1896d388.js"
+  "/goit-react-hw-04-pet-store/precache-manifest.705a0b283c0d22ef4fa4c090e0cea757.js"
 );
 
 self.addEventListener('message', (event) => {
